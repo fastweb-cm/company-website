ad134e37f71c8ea9529bebd66eba92519810a215
ichami630 <brandonichami@gmail.com>
2025-11-17 18:08:10 +0100

    removed the api folder

--- api/src/controllers/action_send_email.php
+++
@@ -1,7 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Credentials: true");
-
-echo "conecting...";
