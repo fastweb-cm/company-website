7b0c3b82d6c7afe53dd11a796b50a571d4137f21
Phyna <josephineeho8@gmail.com>
2026-02-26 12:50:43 +0100

    merge

--- app/src/pages/Login.jsx
+++ app/src/pages/Login.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Button from '../components/Button';
 
 const Login = () => {
   return (
     <div>
+        <img src="/logo.png" alt="" className='w-xl rounded mx-auto'/>
+
       <h2 className='text-2xl font-bold
       text-center mb-6 text-gray-800'>Welcome Back!</h2>
 
@@ -19,20 +22,13 @@ const Login = () => {
        className=' text-sm text-red-600 hover:underline'>Forgot Password?</Link>
     </div>
 
-    <button className='w-full bg-red-700 text-white p-3 rounded-2xl
-    font-semibold hover:bg-red-800 transition'>Login</button>
+    <Button variant='primary' hoverBg='black'className='w-full' >Login</Button>
     
 
     <div className='flex items-center my-6'>
-      <div className='flex-grow h-px bg-gray-300'></div>
-      <span className='px-3 text-gray-400 text-sm'>OR</span>
+      <div className='flex-grow h-px  bg-gray-300'></div>
       <div className='flex-grow h-px bg-gray-300'></div>
     </div>
-    
-
-    <p className='text-center text-gray-900 text-sm '>Don't have an account? <Link to="/auth/signup" className='text-red-600 hover:underline'>Sign Up</Link></p>
-    {/* <span className='text-blue-500 cursor-pointer hover:underline'>Sign up</span> */}
-    
     </div>
   );
 };
