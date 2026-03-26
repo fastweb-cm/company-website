a3cefa62e390eacc7d5cdf4f2783416dc4f6bc9e
ichami630 <brandonichami@gmail.com>
2026-03-26 16:00:31 +0100

    complete crud operation for blogpost

--- backend/app/Core/Controller.php
+++ backend/app/Core/Controller.php
@@ -2,14 +2,58 @@
 namespace App\Core;
 
 class controller {
-    protected function json($data, int $statusCode = 200): void {
+    protected static function json($data, int $statusCode = 200, bool $exit = true): void {
         http_response_code($statusCode);
         header('Content-Type: application/json');
         echo json_encode($data);
+        if($exit) exit;
     }
 
+    protected function request(): string
+    {
+        return $_SERVER['REQUEST_METHOD'] ?? 'GET';
+    }
     protected function getJsonInput(): array {
         return json_decode(file_get_contents('php://input'), true) ?? [];
+    }
+
+    protected function getInput(): array
+    {
+        $contentType = $_SERVER["CONTENT_TYPE"] ?? '';
+
+        if (str_contains($contentType, 'application/json')) {
+            return json_decode(file_get_contents('php://input'), true) ?? [];
+        }
+
+        if (str_contains($contentType, 'multipart/form-data')) {
+            return $_POST; // already parsed by PHP
+        }
+
+        return [];
+    }
+
+    /**
+     * Get query parameters from the current request
+     * Returns an associative array
+    */
+    protected function getQueryParams(): array {
+        return  $_GET ?? [];
+    }
+
+    protected static function getAuthHeader(): string
+    {
+        // $headers = getallheaders(); //fetches all headers sent from this request
+        $headers = getallheaders();
+        // error_log(json_encode($headers));
+
+        if (!isset($headers["Authorization"])) {
+            self::json([
+                "status" => 'error',
+                "message" => "Access Denied"
+            ],401);
+        }
+
+        return str_replace("Bearer ", '', $headers['Authorization']);   //return the bearer token
     }
 }
 ?>
